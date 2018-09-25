2ba7acf884f00a32d743e8e6c25fd31fc4ddbd66
omarhoblos <omar@simpatico.ai>
2018-09-25 15:02:16 -0400

    added getters & setters to question & questionnaire objects

--- src/app/service/questionnaire.service.ts
+++ src/app/service/questionnaire.service.ts
@@ -15,19 +15,6 @@ export interface Code {
   display: string;
 }
 
-export interface Item2 {
-  linkId: string;
-  text: string;
-  type: string;
-}
-
-export interface Item {
-  linkId: string;
-  text: string;
-  type: string;
-  item: Item2[];
-}
-
 export interface RootObject {
   resourceType: string;
   id: string;
@@ -40,29 +27,130 @@ export interface RootObject {
   item: Item[];
 }
 
-class QuestionItem {
-  constructor () {}
+class Item {
+  private linkId: string; private text: string; private type: string;
+  get LinkId(): string {
+    return this.linkId;
+  }
+
+  getText(): string {
+    return this.text;
+  }
+
+  getType(): string {
+    return this.type;
+  }
+
+  setLinkId(linkId: string) {
+    this.linkId = linkId;
+  }
+
+  setText(text: string) {
+    this.text = text;
+  }
+
+  setType(type: string) {
+    this.type = type;
+  }
 }
 
 class Question {
-  constructor (public resourceType: string,
-    public id: string, public url: string, public date: string,
-    public code: Code, public subjectType: string[], public item: Item[]) {}
+  private resourceType: string;
+  private id: string;
+  private url: string;
+  private date: Date;
+  private code: Code;
+  private subjectType: string[];
+  private item: Item[];
+
+  getResourceType() {
+    return this.resourceType;
+  }
+
+  setResourceType(resourceType: string) {
+    this.resourceType = resourceType;
+  }
+
+  getId() {
+    return this.id;
+  }
+
+  setId(id: string) {
+    this.id = id;
+  }
+
+  getUrl() {
+    return this.url;
+  }
+
+  setUrl(url: string) {
+    this.url = url;
+  }
+
+  getDate() {
+    return this.date;
+  }
+
+  setDate(date: Date) {
+    this.date = date;
+  }
+
+  getCode() {
+    return this.code;
+  }
+
+  setCode(code: Code) {
+    this.code = code;
+  }
+
+  getSubjectType() {
+    for (const i of this.subjectType) {
+      return i;
+    }
+  }
+
+  setSubjectType (subjectType: string) {
+    this.subjectType.push(subjectType);
+  }
+
+  getItem() {
+    for (const i of this.item) {
+      return i;
+    }
+  }
+
+  setItem(item: Item) {
+    this.item.push(item);
+  }
+
 }
 
 class Quesntionnaire {
-  static create(question: Question) {
-    return new Question(question.resourceType, question.id, question.url,
-    question.date, question.code, question.subjectType, question.item);
+  private questions: Question[];
+
+  getQuestions() {
+    for (const i of this.questions) {
+      return i;
+    }
   }
+
+  setQuestions(question: Question) {
+    this.questions.push(question);
+  }
+}
+
+class QuesntionnaireResposne {
+  
 }
 
 export class QuestionnaireService {
 
+  rootObject: RootObject;
+
   constructor(private httpClient: HttpClient, private oauthService: OAuthService) { }
   getQuestionnaireData(pid) {
-    return this.httpClient.get<JSON>(environment.queryURI +
-      '/Quesntionnaire/' + pid, { headers: this.getHeaders() }).subscribe(data => console.log(data));
+    return this.httpClient.get(environment.queryURI +
+      '/Quesntionnaire/' + pid, { headers: this.getHeaders() }).subscribe(res => console.log(res));
 
     // return this._http.get(environment.queryURI + '/Patient/' + pid, { headers: this.getHeaders() });
   }
@@ -72,6 +160,7 @@ export class QuestionnaireService {
       '/Questionnaire/', { headers: this.getHeaders() }).subscribe(data => console.log(data));
 
   }
+
 
 
 
