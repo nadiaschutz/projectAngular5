e4cf1ba17a9109feafc0e4b1b16ded2237cc0634
Lalith Aditya Goga <gogalalith@gmail.com>
2018-11-20 13:34:45 -0500

    more changes

--- src/app/components/district-office/district-office.component.ts
+++ src/app/components/district-office/district-office.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-district-office',
@@ -7,21 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DistrictOfficeComponent implements OnInit {
 
-  districtOffice = '';
-  regionalOffice = '';
-  email = '';
-  phoneNumber = '';
-  faxNumber = '';
-  address = '';
-  city = '';
-  province = '';
-  postalCode = '';
+  regionalOffice = ['Atlantic', 'Quebec', 'NCR', 'Ontario', 'Prairies', 'Pacific'];
 
-  constructor() { }
+  districtOffices = [];
+  locationFormGroup: FormGroup;
+  showFormElement = false;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.showFormElement = false;
+    this.fetchAllDistrictOffices();
+    this.locationFormGroup = this.formBuilder.group({
+      resourceType: 'Location',
+      type: ['', [Validators.required]],
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      phoneNumber: ['', [Validators.required]],
+      faxNumber: ['', [Validators.required]],
+      addressStreet: ['', [Validators.required]],
+      addressCity: ['', [Validators.required]],
+      addressProvince: ['', [Validators.required]],
+      addressPostalCode: ['', [Validators.required]],
+      id: ['', [Validators.required]],
+      managingOrganization: ['', [Validators.required]]
+    });
+    console.log(this.locationFormGroup);
   }
 
-  addNewDistrictOffice() {}
+  addNewDistrictOffice() {
+    this.showFormElement = true;
+    console.log(this.locationFormGroup);
+  }
+
+  editNewDistrictOffice() {
+    this.showFormElement = true;
+  }
+
+  fetchAllDistrictOffices() {
+    // Http call to fetch all offices
+  }
 
 }
