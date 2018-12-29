9b659bb11daf8ead423c85bdf8a920e98223eab0
nadia <nadiaikon@gmail.com>
2018-12-29 10:38:52 +0000

    add PSOHP office page is finished, add client department page is 50% done

--- src/app/components/client-department/client-department.component.ts
+++ src/app/components/client-department/client-department.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { UserService } from '../../service/user.service';
 import {
   FormBuilder,
@@ -9,45 +9,192 @@ import {
 import { HttpClient } from '@angular/common/http';
 import * as FHIR from '../../interface/FHIR';
 import { Router } from '@angular/router';
+import { DynamicFormComponent } from '../dynamic-forms/dynamic-form.component';
+import { FieldConfig } from '../dynamic-forms/field-config.interface';
 
 @Component({
   selector: 'app-client-department',
   templateUrl: './client-department.component.html',
   styleUrls: ['./client-department.component.scss']
 })
-export class ClientDepartmentComponent implements OnInit {
-  clientDepartmentFormGroup: FormGroup;
-  departments = [];
-  regionalOffices = [];
-  regionalOfficesWithId = {};
-  clientDepartments = [];
-  addClientDepartment = false;
-  clientDepartmentName = '';
-  clientDepartmentCreationSuccess = false;
+export class ClientDepartmentComponent implements OnInit, AfterViewInit {
 
-  constructor(
-    private userService: UserService,
-    private formBuilder: FormBuilder,
-    private router: Router
-  ) {}
+
+      // var for styling each form field
+      style = 'col-5';
+
+
+      clientDepartmentFormGroup: FormGroup;
+      departments = [];
+      regionalOffices = [];
+      regionalOfficesWithId = {};
+      clientDepartments = [];
+      addClientDepartment = false;
+      clientDepartmentName = '';
+      clientDepartmentCreationSuccess = false;
+
+      constructor(
+        private userService: UserService,
+        private formBuilder: FormBuilder,
+        private router: Router
+      ) {}
+
+
+      @ViewChild(DynamicFormComponent) form: DynamicFormComponent;
+      config: FieldConfig[] = [
+        {
+          type: 'input',
+          label: 'Department Branch',
+          inputType: 'text',
+          placeholder: 'Enter Department Branch',
+          name: 'branch',
+          validation: [Validators.required]
+        },
+        {
+          type: 'select',
+          label: 'PSOHP Region',
+          name: 'region',
+          options: [],
+          placeholder: 'Select Region',
+          validation: [Validators.required]
+        },
+        {
+          type: 'select',
+          label: 'Department Name',
+          name: 'department',
+          options: [],
+          placeholder: 'Select Department Name',
+          validation: [Validators.required]
+        },
+        {
+          type: 'header',
+          label: 'Don\'t see a Client Department in this list? to add one'
+        },
+        {
+          type: 'input',
+          label: 'Phone Number',
+          inputType: 'text',
+          placeholder: 'Enter Phone Number',
+          name: 'phone',
+          validation: [
+            Validators.required,
+            // CustomValidator.numberValidator,
+            Validators.pattern('^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$')
+          ]
+        },
+        {
+          type: 'input',
+          label: 'Fax Number',
+          inputType: 'text',
+          placeholder: 'Enter Fax Number',
+          name: 'fax',
+          validation: [
+            Validators.required,
+            // CustomValidator.numberValidator,
+            Validators.pattern('^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$')
+          ]
+        },
+        {
+          type: 'input',
+          label: 'Email Address',
+          inputType: 'email',
+          placeholder: 'Enter Email',
+          name: 'email',
+          validation: [Validators.required, Validators.email]
+        },
+        {
+          type: 'input',
+          label: 'Address',
+          inputType: 'text',
+          placeholder: 'Enter Address',
+          name: 'address',
+          validation: [Validators.required]
+        },
+        {
+          type: 'input',
+          label: 'City',
+          inputType: 'text',
+          placeholder: 'Enter City',
+          name: 'city',
+          validation: [Validators.required]
+        },
+        {
+          type: 'input',
+          label: 'Province',
+          inputType: 'text',
+          placeholder: 'Enter Province',
+          name: 'province',
+          validation: [Validators.required]
+        },
+        {
+          type: 'input',
+          label: 'Postal Code',
+          inputType: 'text',
+          placeholder: 'Enter Postal Code',
+          name: 'postalCode',
+          validation: [Validators.required]
+        },
+        {
+          type: 'checkbox',
+          label: 'Chargeback Client',
+          name: 'ChargebackClient',
+          validation: [Validators.required]
+        },
+        {
+          type: 'line'
+        },
+        {
+          type: 'button',
+          name: 'submit',
+          label: 'Save'
+        }
+      ];
+
+      ngAfterViewInit() {
+        setTimeout(() => {
+        let previousValid = this.form.valid;
+        this.form.changes.subscribe(() => {
+          if (this.form.valid !== previousValid) {
+            previousValid = this.form.valid;
+            this.form.setDisabled('submit', !previousValid);
+          }
+
+        });
+
+        this.form.setDisabled('submit', true);
+
+      });
+
+      // if you want to style 2 form fields per a row do these :
+      this.wrap();
+      this.addDiv();
+      // the end
+
+      }
+
+      submit(value: {[name: string]: any}) {
+        console.log(value);
+      }
+
+    wrap() {
+      const x = $('.field-holder-2 form-input');
+      for (let i = 0; i < x.length; i ++) {
+        console.log(x[i]);
+        $(x[i]).wrap("<div class='" + this.style +"'></div>");
+      }
+    }
+
+      addDiv() {
+        const sections = $('.dynamic-form .' + this.style);
+        for (let i = 0; i < sections.length; i += 2) {
+        sections.slice(i, i + 2).wrapAll("<div class='row'></div>");
+      }
+    }
+
 
   ngOnInit() {
     this.fetchAllRegionalOffices();
     this.fetchAllClientDepartments();
-    this.clientDepartmentFormGroup = this.formBuilder.group({
-      psohpRegion: new FormControl('', [Validators.required]),
-      departmentName: new FormControl('', [Validators.required]),
-      departmentBranch: new FormControl('', [Validators.required]),
-      contactName: new FormControl('', [Validators.required]),
-      chargebackClient: new FormControl(''),
-      email: new FormControl('', [Validators.required, Validators.email]),
-      phoneNumber: new FormControl('', [Validators.required]),
-      faxNumber: new FormControl('', [Validators.required]),
-      addressStreet: new FormControl('', [Validators.required]),
-      addressCity: new FormControl('', [Validators.required]),
-      addressProvince: new FormControl('', [Validators.required]),
-      addressPostalCode: new FormControl('', [Validators.required])
-    });
   }
 
   setClientDepartment() {
