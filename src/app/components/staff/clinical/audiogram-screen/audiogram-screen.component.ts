e06b33aa799c83bc37286b51bf7ef6e5d8f392f9
O Hob <m6jvq@vmani.com>
2019-02-25 17:53:31 -0500

    added new components for immunizations and audiogram, added styling to checklist for immunizations

---
+++ src/app/components/staff/clinical/audiogram-screen/audiogram-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-audiogram-screen',
+  templateUrl: './audiogram-screen.component.html',
+  styleUrls: ['./audiogram-screen.component.scss']
+})
+export class AudiogramScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/staff/clinical/immunization-screen/immunization-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-immunization-screen',
+  templateUrl: './immunization-screen.component.html',
+  styleUrls: ['./immunization-screen.component.scss']
+})
+export class ImmunizationScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
