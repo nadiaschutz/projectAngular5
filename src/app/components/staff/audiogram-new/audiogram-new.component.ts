a00bd2be993d086499b4c12ff5501d449e9cd4fd
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2019-03-05 09:57:14 +0530

    audio gram form save completed

--- src/app/components/staff/audiogram-new/audiogram-new.component.ts
+++ src/app/components/staff/audiogram-new/audiogram-new.component.ts
@@ -6,6 +6,8 @@ import { AudiogramService } from '../../../service/audiogram.service';
 import { UtilService } from '../../../service/util.service';
 
 import * as moment from 'moment';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import * as FHIR from '../../../interface/FHIR';
 
 @Component({
   selector: 'app-audiogram-new',
@@ -13,8 +15,10 @@ import * as moment from 'moment';
   styleUrls: ['./audiogram-new.component.scss']
 })
 export class AudiogramNewComponent implements OnInit {
-  selectedHearingLoss: any;
-  selectedBaseLine: any;
+    audioGramFormGroup: FormGroup;
+    DATE_FORMAT = 'YYYY-MM-DD';
+  selectedHearingLoss = null;
+  selectedBaseLine = null;
   datePickerConfig: Partial<BsDatepickerConfig>;
   formValue = {
     dateOfTest : null,
@@ -48,11 +52,41 @@ export class AudiogramNewComponent implements OnInit {
   private eoc: any;
   private patient: any;
   private serviceRequest: any;
+  locations: any;
 
   constructor(private route: ActivatedRoute,
               private router: Router,
+              private formBuilder: FormBuilder,
               private audiogramService: AudiogramService,
-              private utilService: UtilService) { }
+              private utilService: UtilService) {
+
+      this.audioGramFormGroup = this.formBuilder.group({
+          issued: new FormControl(''),
+          locationOfTest: new FormControl(''),
+          devSerial: new FormControl(''),
+          devModel: new FormControl(''),
+          devCelibrationDate: new FormControl(''),
+          examineID: new FormControl(''),
+
+          leftk_5: new FormControl('' , Validators.required),
+          leftk1: new FormControl('' , Validators.required),
+          leftk2: new FormControl('', Validators.required),
+          left3k: new FormControl('', Validators.required),
+          left4k: new FormControl('', Validators.required),
+          left6k: new FormControl('', Validators.required),
+          left8k: new FormControl('' , Validators.required),
+
+          right_0_5: new FormControl('', Validators.required),
+          right_1k: new FormControl('' , Validators.required),
+          right_2k: new FormControl('' , Validators.required),
+          right_3k: new FormControl('' , Validators.required),
+          right_4k: new FormControl('', Validators.required),
+          right_6k: new FormControl('', Validators.required),
+          right_8k: new FormControl('', Validators.required),
+
+
+      });
+  }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
@@ -68,6 +102,30 @@ export class AudiogramNewComponent implements OnInit {
               this.eoc = resource;
             } else if (resource.resourceType === 'Patient') {
               this.patient = this.utilService.getPatientJsonObjectFromPatientFhirObject(resource);
+
+
+              if(this.patient && this.patient.workplace){
+
+                  this.audiogramService.getBundleFromOrganizationName(this.patient.workplace).subscribe(
+                      data => {
+
+                          if (data && data['entry'] && Array.isArray(data['entry']) && data['entry'].length > 0) {
+                              data['entry'].map(item => {
+                                  const resource = item.resource;
+                                  if (resource.resourceType === 'Organization') {
+                                   this.getLocations(resource.resourceType , resource.id);
+                                  }
+                              });
+                          }
+
+
+                      },
+                      error => this.handleError(error)
+                  );
+              }
+
+
+
             } else if (resource.resourceType === 'QuestionnaireResponse') {
               this.serviceRequest = resource; // qr
             }
@@ -78,6 +136,11 @@ export class AudiogramNewComponent implements OnInit {
         console.log('patient =>', this.patient);
       });
     });
+
+      this.datePickerConfig = Object.assign(
+          {},
+          { containerClass: 'theme-dark-blue', dateInputFormat: this.DATE_FORMAT, rangeInputFormat: this.DATE_FORMAT }
+      );
   }
 
   calculateAge(dateOfBirth) {
@@ -99,9 +162,365 @@ export class AudiogramNewComponent implements OnInit {
     this.formValue.baseLine = type;
   }
 
+    getLocations(resourceType , resourseId) {
+
+        this.audiogramService.getLocations(resourceType , resourseId).subscribe(
+            data => {
+                if(data && data['entry'] && data['entry'].length > 0){
+                    this.locations = data['entry'];
+                }
+
+
+               console.log("this.locationsthis.locationsthis.locations",this.locations)
+            }
+    ,
+            error => this.handleError(error)
+        );
+   }
+
   saveAudiogram(){
-    console.log(this.formValue);
-    this.router.navigateByUrl('/staff/audiogram/' + this.eocId);
+
+      const deviceRequest  = {
+          "resourceType": "Device",
+          "udi": {
+              "deviceIdentifier": this.audioGramFormGroup.get('devSerial').value
+          },
+          "model": this.audioGramFormGroup.get('devModel').value,
+          "note": [
+              {
+                  "time": moment(this.audioGramFormGroup.get('devCelibrationDate').value).format(),
+                  "text": "calibration-date"
+              }
+          ]
+      };
+
+
+      this.audiogramService.postDevice(JSON.stringify(deviceRequest)).subscribe(deviceData => {
+
+          const audiogramRequest = {
+              resourceType : null,
+              extension : [],
+              status: null,
+              category: [],
+              subject: {
+                  reference: null
+              },
+              context: {
+                  reference : null
+              },
+              issued: null,
+              device: {
+                  reference : null
+              },
+              component : []
+          };
+          if(deviceData){
+
+
+          audiogramRequest.resourceType = 'Observation';
+          audiogramRequest.extension = [
+              {
+                  url: "examiner",
+                  valueString: this.audioGramFormGroup.get('examineID').value
+              },
+              {
+                  "url": "test-location",
+                  "valueReference": {
+                      "reference": "Location/"+ this.audioGramFormGroup.get('locationOfTest').value
+                  }
+              }
+          ];
+          audiogramRequest.status = 'final';
+          audiogramRequest.category = [
+              {
+                  "coding": [
+                      {
+                          "system": "http://hl7.org/fhir/observation-category",
+                          "code": "exam",
+                          "display": "Exam"
+                      }
+                  ],
+                  "text": "Exam"
+              },
+              {
+                  "coding": [
+                      {
+                          "system": "http://nohis.gc.ca/observation-type",
+                          "code": "audiogram",
+                          "display": "Audiogram"
+                      }
+                  ],
+                  "text": "Audiogram"
+              },
+
+          ];
+          if(this.selectedBaseLine === 'yes'){
+              let componenet = {
+                  "coding": [
+                      {
+                          "system": "http://nohis.gc.ca/observation-use",
+                          "code": "baseline",
+                          "display": "Baseline"
+                      }
+                  ],
+                  "text": "Baseline"
+              };
+
+              audiogramRequest.category.push(componenet);
+          }else {
+              audiogramRequest.category = audiogramRequest.category.filter(data =>{
+                  if(data.text  !== "Baseline") {
+                      return data
+                  }
+              });
+          }
+          let selectHearingLoss;
+
+          if(this.selectedHearingLoss === 'left'){
+              selectHearingLoss = {
+                  "coding": [
+                      {
+                          "system": "http://nohis.gc.ca/hearing-loss",
+                          "code": "hearing-loss",
+                          "display": "Hearing Loss"
+                      }
+                  ],
+                  "text": "Left"
+              }
+          }else {
+              selectHearingLoss = {
+                  "coding": [
+                      {
+                          "system": "http://nohis.gc.ca/hearing-loss",
+                          "code": "hearing-loss",
+                          "display": "Hearing Loss"
+                      }
+                  ],
+                  "text": "Right"
+              }
+          }
+          if(this.selectedHearingLoss !== null){
+              audiogramRequest.category.push(selectHearingLoss);
+          }
+
+
+          audiogramRequest.subject = {
+              "reference": "Patient/"+ this.patient.id
+          };
+          audiogramRequest.context = {
+              "reference": "EpisodeOfCare/"+ this.eocId
+          };
+          audiogramRequest.issued =  this.audioGramFormGroup.get('issued').value;
+          audiogramRequest.device = {
+              "reference": "Device/" + deviceData['id']
+          };
+
+          audiogramRequest.component = [
+              {
+                  "code": {
+                      "coding": [
+                          {
+                              "system": "http://loinc.org",
+                              "code": "89024-4",
+                              "display": "Hearing threshold 500 Hz Ear-L"
+                          }
+                      ]
+                  },
+                  "valueString": this.audioGramFormGroup.get('leftk_5').value
+              },
+              {
+                  "code": {
+                      "coding": [
+                          {
+                              "system": "http://loinc.org",
+                              "code": "89025-1",
+                              "display": "Hearing threshold 500 Hz Ear-R"
+                          }
+                      ]
+                  },
+                  "valueString": this.audioGramFormGroup.get('right_0_5').value
+              },
+              {
+                  "code": {
+                      "coding": [
+                          {
+                              "system": "http://loinc.org",
+                              "code": "89016-0",
+                              "display": "Hearing threshold 1000 Hz Ear-L"
+                          }
+                      ]
+                  },
+                  "valueString": this.audioGramFormGroup.get('leftk1').value
+              },
+              {
+                  "code": {
+                      "coding": [
+                          {
+                              "system": "http://loinc.org",
+                              "code": "89017-8",
+                              "display": "Hearing threshold 1000 Hz Ear-R"
+                          }
+                      ]
+                  },
+                  "valueString": this.audioGramFormGroup.get('right_1k').value
+              },
+              {
+                  "code": {
+                      "coding": [
+                          {
+                              "system": "http://loinc.org",
+                              "code": "89018-6",
+                              "display": "Hearing threshold 2000 Hz Ear-L"
+                          }
+                      ]
+                  },
+                  "valueString": this.audioGramFormGroup.get('leftk2').value
+              },
+              {
+                  "code": {
+                      "coding": [
+                          {
+                              "system": "http://loinc.org",
+                              "code": "89019-4",
+                              "display": "Hearing threshold 2000 Hz Ear-R"
+                          }
+                      ]
+                  },
+                  "valueString": this.audioGramFormGroup.get('right_2k').value
+              },
+              {
+                  "code": {
+                      "coding": [
+                          {
+                              "system": "http://loinc.org",
+                              "code": "89020-2",
+                              "display": "Hearing threshold 3000 Hz Ear-L"
+                          }
+                      ]
+                  },
+                  "valueString": this.audioGramFormGroup.get('left3k').value
+              },
+              {
+                  "code": {
+                      "coding": [
+                          {
+                              "system": "http://loinc.org",
+                              "code": "89021-0",
+                              "display": "Hearing threshold 3000 Hz Ear-R"
+                          }
+                      ]
+                  },
+                  "valueString": this.audioGramFormGroup.get('right_3k').value
+              },
+              {
+                  "code": {
+                      "coding": [
+                          {
+                              "system": "http://loinc.org",
+                              "code": "89022-8",
+                              "display": "Hearing threshold 4000 Hz Ear-L"
+                          }
+                      ]
+                  },
+                  "valueString": this.audioGramFormGroup.get('left4k').value
+              },
+              {
+                  "code": {
+                      "coding": [
+                          {
+                              "system": "http://loinc.org",
+                              "code": "89023-6",
+                              "display": "Hearing threshold 4000 Hz Ear-R"
+                          }
+                      ]
+                  },
+                  "valueString": this.audioGramFormGroup.get('right_4k').value
+              },
+              {
+                  "code": {
+                      "coding": [
+                          {
+                              "system": "http://loinc.org",
+                              "code": "89026-9",
+                              "display": "Hearing threshold 6000 Hz Ear-L"
+                          }
+                      ]
+                  },
+                  "valueString": this.audioGramFormGroup.get('left6k').value
+              },
+              {
+                  "code": {
+                      "coding": [
+                          {
+                              "system": "http://loinc.org",
+                              "code": "89027-7",
+                              "display": "Hearing threshold 6000 Hz Ear-R"
+                          }
+                      ]
+                  },
+                  "valueString": this.audioGramFormGroup.get('right_6k').value
+              },
+              {
+                  "code": {
+                      "coding": [
+                          {
+                              "system": "http://loinc.org",
+                              "code": "89028-5",
+                              "display": "Hearing threshold 8000 Hz Ear-L"
+                          }
+                      ]
+                  },
+                  "valueString": this.audioGramFormGroup.get('left8k').value
+              },
+              {
+                  "code": {
+                      "coding": [
+                          {
+                              "system": "http://loinc.org",
+                              "code": "89029-3",
+                              "display": "Hearing threshold 8000 Hz Ear-R"
+                          }
+                      ]
+                  },
+                  "valueString": this.audioGramFormGroup.get('right_8k').value
+              }
+          ];
+
+
+
+
+          console.log(JSON.stringify(audiogramRequest));
+
+          this.audiogramService.saveAudiogramRequest(JSON.stringify(audiogramRequest)).subscribe(data => {
+
+
+             console.log("Audiogram Response", data)
+
+
+              this.router.navigateByUrl('/staff/audiogram/' + this.eocId);
+          });
+
+
+          }
+
+      });
+
+
+
+   // this.router.navigateByUrl('/staff/audiogram/' + this.eocId);
   }
+
+    handleError(error) {
+        console.log(error);
+    }
+
+    onChangeLocation(location) {
+        const selectedLocation = JSON.parse(location);
+        if (selectedLocation) {
+            console.log(selectedLocation);
+        }
+
+    }
+
 
 }
--- src/app/service/audiogram.service.ts
+++ src/app/service/audiogram.service.ts
@@ -23,4 +23,29 @@ export class AudiogramService {
     });
     return headers;
   }
+
+    getPostHeaders(): HttpHeaders {
+        const headers = new HttpHeaders({
+            'Content-Type' : 'application/json',
+            'Authorization': 'Bearer ' + this.oauthService.getAccessToken()
+        });
+        return headers;
+    }
+
+  postDevice(deviceRequestData) {
+      return this.http.post(environment.queryURI + '/Device', deviceRequestData, {headers: this.getPostHeaders()});
+  }
+
+  getBundleFromOrganizationName(workplace) {
+    return this.http.get(environment.queryURI + '/Organization?name=' /*+ workplace*/ + 'Canadian Coast Guard (CCG)'  , { headers: this.getHeaders() });
+  }
+
+    getLocations(resourceType , id) {
+        return this.http.get<JSON>(environment.queryURI +
+            '/Location?type=DEPTBRANCH&organization=' + resourceType + '/' + id , { headers: this.getHeaders() });
+    }
+
+    saveAudiogramRequest(audiogramRequestData) {
+        return this.http.post(environment.queryURI + '/Observation', audiogramRequestData, {headers: this.getPostHeaders()});
+    }
 }
