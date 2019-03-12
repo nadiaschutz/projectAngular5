04b6d92fe8efc329a36445a3ae7c8b799475a58a
O Hob <m6jvq@vmani.com>
2019-03-12 15:18:02 -0400

    added experimental changes to list page

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+  base_href: '',
+  domain: 'http://localhost:4200',
+  issuer: 'https://bcip.smilecdr.com/smartauth',
+  loginUrl: 'http://localhost:4200',
+  queryURI: 'https://bcip.smilecdr.com/fhir-request',
+  logoutURI: 'https://bcip.smilecdr.com/smartauth',
+  redirectUri: 'http://localhost:4200',
+  clientId: 'NOHIS',
+  scope: 'launch/patient openid profile cdr_all_user_authorities',
+  scopeUrl: 'launch%2Fpatient%20openid%20profile%20cdr_all_user_authorities',
+  loginLink: 'https://bcip.smilecdr.com/smartauth/oauth/token',
+  jsonAPI: 'https://bcip.smilecdr.com/json-admin'
 };
